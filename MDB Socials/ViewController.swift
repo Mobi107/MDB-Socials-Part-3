18ae0659074b5cd63910dec6eb537641055fa6f3
Mudabbir Khan <m.khan@berkeley.edu>
2018-02-21 04:52:59 -0800

    Initial Commit

---
+++ MDB Socials/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MDB Socials
+//
+//  Created by Mudabbir Khan on 2/21/18.
+//  Copyright © 2018 MHK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
