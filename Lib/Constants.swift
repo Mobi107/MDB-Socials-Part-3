498c7541cdf9eb1b37ad2fce5ba27c4dec7f5b62
Mudabbir Khan <m.khan@berkeley.edu>
2018-03-03 23:55:26 -0800

    refactor

---
+++ Lib/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MDB Socials
+//
+//  Created by Mudabbir Khan on 3/3/18.
+//  Copyright © 2018 MHK. All rights reserved.
+//
+
+import Foundation
+import ChameleonFramework
+
+struct Constants {
+    static let appColor = UIColor(hexString: "#2B99E2")
+}
